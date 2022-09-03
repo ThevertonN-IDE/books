073161770292643fd14ad9a895567f7772a33dd2
ThevertonN-IDE <84247201+ThevertonN-IDE@users.noreply.github.com>
2022-09-03 19:26:10 -0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,4 @@
+var nome = window.prompt('Digite seu nome:')
+document.write(`<h1>Bem-vindo, ${nome}</h1>`)
+var num = Number(window.prompt('Digite um número:'))
+document.writeln(`O número digitado foi: ${num}`)
